5f6d1a4a6e7e003262d0f8cd1c1d456881b489e6
akulenovic97 <akulenovic97@gmail.com>
2020-12-15 20:06:26 -0500

    add postgres DB to settings

--- customer_api/customer_api/settings.py
+++ customer_api/customer_api/settings.py
@@ -107,10 +107,15 @@ WSGI_APPLICATION = 'customer_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#Using postgres for this app (psycopg2)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'customer_db',
+        'USER': 'admin',
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
